fd571088b110c74c590d9f0350ea47f5fa80a88a
Subleader <baker.leader@gmail.com>
2018-10-11 14:20:14 +0200

    Ajout liste des entreprises

--- AppliStage/src/Controller/AccueilController.php
+++ AppliStage/src/Controller/AccueilController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
-class AccueilController extends AbstractController
+class AccueilController extends Controller
 {
     /**
      * @Route("/accueil", name="accueil")
      */
     public function index()
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]);
+			$entreprises = $this->getDoctrine()
+            ->getRepository(Entreprise::class)
+            ->findAll();
+        return $this->render('accueil/index.html.twig', compact('entreprises'));
     }
+	
+	
 }
--- applistage/src/Controller/IndexController.php
+++ applistage/src/Controller/IndexController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
-class IndexController extends AbstractController
+class IndexController extends Controller
 {
     /**
      * @Route("/", name="index")
